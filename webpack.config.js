496b861f87b228ad5918a7ccedc19917ab1a4fac
Tyom Semonov <inspiro@gmail.com>
2016-04-04 13:24:11 +0100

    Add project files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: {
+    index: [
+      './source/javascripts/index.js'
+    ]
+  },
+
+  devtool: 'inline-source-map',
+
+  output: {
+    path: __dirname + '/.tmp/dist',
+    filename: 'javascripts/bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
