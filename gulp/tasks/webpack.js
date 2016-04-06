4d882a8645bf23b42213bcd293559563c0359318
Tyom Semonov <inspiro@gmail.com>
2016-04-06 19:15:13 +0100

    Hide chunks stats in Gulp webpack task

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -8,7 +8,10 @@ gulp.task('webpack', ['clean:js'], done => {
   return webpack(require(paths.webpackConfig)).run((err, stats) => {
     if (err) { throw new gutil.PluginError("webpack", err) }
 
-    gutil.log("[webpack]", stats.toString({colors: true}));
+    gutil.log("[webpack]", stats.toString({
+      colors: true,
+      chunks: false
+    }));
 
     browserSync.reload();
     return done();
