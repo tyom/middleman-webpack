9d7bc41439ad050af746d9f3bb389e3a8e237c0f
Tyom Semonov <inspiro@gmail.com>
2016-04-06 18:41:13 +0100

    Remove `files` from Browser Sync init

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -4,7 +4,6 @@ const browserSync = require('browser-sync').create('bs-proxy');
 gulp.task('browser-sync', function() {
   browserSync.init({
     proxy: 'localhost:4567',
-    files: 'stylesheets/bundle.css',
     reloadDelay: 1000,
     open: false
   });
